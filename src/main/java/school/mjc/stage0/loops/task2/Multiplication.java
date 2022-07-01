fa52ba339e36f38845a11f6dc16271343050b75e
Roman Gulevich <R.I.Gulevich@gmail.com>
2022-07-01 19:38:57 +0300

    "Module loop - task2"

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int count = 0;
+        while (count <= Math.abs(multiplyByAndToInclusive)) {
+            if (multiplyByAndToInclusive == 0) {
+                System.out.print("");
+                break;
+            } else {
+                System.out.println(count * multiplyByAndToInclusive);
+                count++;
+            }
+        }
     }
 }
